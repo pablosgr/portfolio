ee9bb0d3e72b27650f7d1d889e26cff8183bc6f7
Pablo <pablosgar96@gmail.com>
2025-01-30 21:37:59 +0100

    v0.7

---
+++ app.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const boton_tlfn = document.querySelector(".phone");
+const boton_email = document.querySelector(".email");
+const boton_cv = document.querySelector(".cv");
+let info_tlfn = document.querySelector(".phone-info");
+let info_email = document.querySelector(".email-info");
+let info_cv = document.querySelector(".cv-info");
+
+boton_tlfn.addEventListener("mouseenter", ()=>{
+    
+})
